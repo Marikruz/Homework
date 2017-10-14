6015538dafee53c3bbdb319743a2794b389b93d1
Marikruz <783165069@qq.com>
2017-10-14 16:06:46 +0800

    作业1作业2

---
+++ 作业2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int x=2.2;
+printf("%d\n",x);
+
+short y=0x7FFF;
+printf("%d\n",y);
+
+short z=0xFFFF;
+printf("%d\n",z);
+
+unsigned short w=0xFFFF;
+printf("%u\n",w);
+
+float u=0.177;
+printf("%f\n",u);
+}
+
