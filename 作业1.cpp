74b7894990236c0195275a32e861ccc2376dccf6
Marikruz <32090491+Marikruz@users.noreply.github.com>
2017-10-21 00:17:44 +0800

    Update 作业1.cpp

--- 作业1.cpp
+++ 作业1.cpp
@@ -1,19 +1,26 @@
 #include<stdio.h>
 
- main()
-
-{
-
-	double a,b,c,d,m,n,x,y;
-
-	printf("请输入x y前面的系数以及m n的值\n");
-
+int main()
+{	
+    double function1(double a,double b,double c,double d,double m,double n,double x,double y),
+           function2(double a,double b,double c,double d,double m,double n,double x,double y);
+	double a,b,c,d,m,n,x,y; 
+	printf("璇疯緭鍏 y鍓嶉潰鐨勭郴鏁颁互鍙妋 n鐨勫�糪n");
 	scanf("%lf%lf%lf%lf%lf%lf",&a,&b,&c,&d,&m,&n);
-
-	x=(m*d-b*n)/(a*d-b*c);
-
-	y=(m*c-a*n)/(b*c-a*d);
-
-	printf("x值为%lf,y值为%lf\n",x,y);
-
+    x=function1(a,b,c,d,m,n,x,y);
+    y=function2(a,b,c,d,m,n,x,y);
+	printf("x鍊间负%lf,y鍊间负%lf\n",x,y);
+	return 0;
  }
+
+double function1(double a,double b,double c,double d,double m,double n,double x,double y)
+{
+	x=(m*d-b*n)/(a*d-b*c);
+	return x;
+}
+
+double function2(double a,double b,double c,double d,double m,double n,double x,double y)
+{
+	y=(m*c-a*n)/(b*c-a*d);
+	return y;
+}
