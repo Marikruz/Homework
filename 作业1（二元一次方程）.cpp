474182b430f026afd5041fb57227e5b467abe896
Marikruz <32090491+Marikruz@users.noreply.github.com>
2017-10-21 16:46:43 +0800

    Update and rename 作业1.cpp to 作业1（二元一次方程）.cpp

--- 作业1（二元一次方程）.cpp
+++ 作业1（二元一次方程）.cpp
@@ -3,14 +3,33 @@
 int main()
 {	
     double function1(double a,double b,double c,double d,double m,double n,double x,double y),
-           function2(double a,double b,double c,double d,double m,double n,double x,double y);
+           function2(double a,double b,double c,double d,double m,double n,double x,double y),e,f,g;
 	double a,b,c,d,m,n,x,y; 
 	printf("请输入x y前面的系数以及m n的值\n");
 	scanf("%lf%lf%lf%lf%lf%lf",&a,&b,&c,&d,&m,&n);
-    x=function1(a,b,c,d,m,n,x,y);
-    y=function2(a,b,c,d,m,n,x,y);
-	printf("x值为%lf,y值为%lf\n",x,y);
-	return 0;
+	e=a/c;
+	f=b/d;
+	g=m/n;
+	if (e==f and f==g)
+	{
+	   printf("有无穷多组解\n");
+       return 0;
+    }
+    else
+    {
+       if (e==f and f!=g)
+       {
+       	  printf("无解\n");
+       	  return 0;
+	   }
+	   else
+	   {
+          x=function1(a,b,c,d,m,n,x,y);
+          y=function2(a,b,c,d,m,n,x,y);
+          printf("x值为%lf,y值为%lf\n",x,y);
+	      return 0;
+	   }
+    }
  }
 
 double function1(double a,double b,double c,double d,double m,double n,double x,double y)
